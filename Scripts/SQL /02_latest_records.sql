169e242ec27bdc680789dffdd75e0e38d86662e0
Ritesh Raju Astekar <riteshastekar40@gmail.com>
2026-09-10 15:20:18 +0530

    Create view for latest trending records

---
+++ Scripts/SQL /02_latest_records.sql
@@ -0,0 +1,9 @@
+CREATE VIEW vw_latest_trending AS
+SELECT * FROM (
+    SELECT *, ROW_NUMBER() OVER (
+        PARTITION BY country,title,channel_title, country
+        ORDER BY trending_date DESC
+    ) AS latest_record
+    FROM country_data
+) o
+WHERE latest_record = 1;
