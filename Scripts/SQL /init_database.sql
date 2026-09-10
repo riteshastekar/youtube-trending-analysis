f1e02f9e6efc58ef6b4aa99520eac7d88b966d91
Ritesh Raju Astekar <riteshastekar40@gmail.com>
2026-09-10 14:59:24 +0530

    Create init_database.sql for YoutubeDB setup
    This script drops the existing 'YoutubeDB' database if it exists and creates a new 'YoutubeDB' database.

---
+++ Scripts/SQL /init_database.sql
@@ -0,0 +1,17 @@
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'YoutubeDB')
+BEGIN
+    ALTER DATABASE YoutubeDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE YoutubeDB;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE YoutubeDB;
+GO
+
+USE YoutubeDB;
+GO
