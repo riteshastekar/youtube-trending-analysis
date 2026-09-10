ab3fa6a5d2dd4c0f75506c37517b894c0fe39bde
Ritesh Raju Astekar <riteshastekar40@gmail.com>
2026-09-10 16:01:27 +0530

    Refactor vw_latest_trending view for latest records

--- Scripts/SQL /02_latest_records.sql
+++ Scripts/SQL /02_latest_records.sql
@@ -1,9 +1,11 @@
 CREATE VIEW vw_latest_trending AS
-SELECT * FROM (
-    SELECT *, ROW_NUMBER() OVER (
-        PARTITION BY country,title,channel_title, country
-        ORDER BY trending_date DESC
-    ) AS latest_record
-    FROM country_data
-) o
+SELECT *
+FROM (
+    SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY content_key
+            ORDER BY trending_date DESC
+        ) AS latest_record
+    FROM vw_country_handlingnulls
+) AS ranked
 WHERE latest_record = 1;
